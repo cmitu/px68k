f97ea9478161c768d7ff7f22295149ce33c68e78
TurtleBazooka <kameya-z@nifty.com>
2022-03-27 12:41:28 +0900

    コメント追加
    わかりやすく記述しましょう。

--- x68k/midi_darwin.h
+++ x68k/midi_darwin.h
@@ -1,4 +1,8 @@
-
+// macOS用CoreMIDI 対応の関数群
+// Win.のMIDI関数とコンパチ(winmm.lib)で置き換えてます。
+// LIST表示して選択するまでもないのでPort0決め打ち出力です。
+// 
+// 2022/3/27  by kameya
 
 #import <CoreMIDI/MIDIServices.h>
 #import <CoreMIDI/CoreMIDI.h>
@@ -25,31 +29,25 @@ typedef	HMIDIOUT *	LPHMIDIOUT;
 #define	MIDI_MAPPER		-1
 #define	CALLBACK_NULL		0x00000000L
 
+// for CoreMIDI 
+	MIDIPortRef mid_port;
+	MIDIClientRef mid_client;
+	MIDIEndpointRef mid_endpoint;
+	MIDIPacket* mid_Packet = 0;
 
-
-	MIDIPortRef m_port;
-	MIDIClientRef m_client;
-	MIDIEndpointRef m_endpoint;
-	MIDIPacket* m_pCurPacket = 0;
 
 	uint8_t MIDI_EXCVWAIT;
-
-//MidiHandler_coremidi(){ m_pCurPacket = 0;}
-
 	const char mid_name[] = {"px68k-MIDI"};
-
-	//const char * GetName(void) { return "coremidi"; }
 
 
 // -----------------------------------------------------------------------
-//   MIDI Port open (Get endpoint Create client , port)
-//   bool MIDI_Open(const char * conf)
+//   MIDI port Open (Client -> Source -> Port ,EndPoint)
+//   select Port0 for output MIDI.
 // -----------------------------------------------------------------------
 uint32_t
 midiOutOpen(LPHMIDIOUT phmo, uint32_t uDeviceID, uint32_t dwCallback,
     uint32_t dwInstance, uint32_t fdwOpen)
 {
-	//phmo; 
 	(void)dwCallback;
 	(void)dwInstance;
 	(void)fdwOpen;
@@ -57,48 +55,48 @@ midiOutOpen(LPHMIDIOUT phmo, uint32_t uDeviceID, uint32_t dwCallback,
 		OSStatus err_sts;
 
 		// Create a MIDI client
-		err_sts = MIDIClientCreate(CFSTR("px68k"), NULL, NULL, &m_client);
+		err_sts = MIDIClientCreate(CFSTR("px68k"), NULL, NULL, &mid_client);
 
 		if (err_sts != noErr)
 		{
-			printf("MIDI:coremidi: No client created.");
+			p6logd("MIDI:CoreMIDI: No client created.");
 			return !MMSYSERR_NOERROR;
 		}
 
 		// Create a MIDI Source
-		err_sts = MIDISourceCreate(m_client, CFSTR("px68k MIDI Source"),
-                     &m_endpoint);
+		err_sts = MIDISourceCreate(mid_client, CFSTR("px68k MIDI Source"),
+					&mid_endpoint);
 
 		if (err_sts != noErr)
 		{
-			printf("MIDI:coremidi: No Sorce created.");
+			p6logd("MIDI:CoreMIDI: No Sorce created.");
 			return !MMSYSERR_NOERROR;
 		}
 
 		// Create a MIDI  port
-		err_sts = MIDIOutputPortCreate(m_client, CFSTR("px68k MIDI Port"), &m_port);
+		err_sts = MIDIOutputPortCreate(mid_client, CFSTR("px68k MIDI Port"), &mid_port);
 
 		if (err_sts != noErr)
 		{
-			printf("MIDI:coremidi: No port created.");
+			p6logd("MIDI:CoreMIDI: No port created.");
 			return !MMSYSERR_NOERROR;
 		}
 
 		// Get the MIDIEndPoint
-		m_endpoint = 0;
+		mid_endpoint = 0;
 		//uint32_t destId = MIDIGetNumberOfDevices();//仮想ポート含まない
-		//m_endpoint = MIDIGetDevice(destId);
+		//mid_endpoint = MIDIGetDevice(destId);
 	    uint32_t destId = MIDIGetNumberOfDestinations();//仮想ポート含む
 		if (destId == 0)
 		{
-			printf("No MIDI-port.\n");
+			p6logd("No MIDI-port found.\n");
 			return !MMSYSERR_NOERROR;
 		}
 		destId = 0;// 最初のMIDI port固定
-		m_endpoint = MIDIGetDestination(destId);
+		mid_endpoint = MIDIGetDestination(destId);
 		CFStringRef strRef;
 		char mididevicename[64];
-		MIDIObjectGetStringProperty(m_endpoint, kMIDIPropertyName, &strRef);
+		MIDIObjectGetStringProperty(mid_endpoint, kMIDIPropertyName, &strRef);
 		CFStringGetCString(strRef, mididevicename, sizeof(mididevicename), 0);
 		CFRelease(strRef);
 		printf("Find MIDI:%s\n",mididevicename);
@@ -118,13 +116,13 @@ midiOutClose(HMIDIOUT hmo)
 	(void)hmo;
 
 	// Dispose the port
-	MIDIPortDispose(m_port);
+	MIDIPortDispose(mid_port);
 
 	// Dispose the client
-	MIDIClientDispose(m_client);
+	MIDIClientDispose(mid_client);
 
 	// Dispose the endpoint
-	MIDIEndpointDispose(m_endpoint);
+	MIDIEndpointDispose(mid_endpoint);
 
 	return MMSYSERR_NOERROR;
 }
@@ -137,30 +135,7 @@ midiOutShortMsg(HMIDIOUT hmo, uint32_t msg)
 {
 	uint8_t messg[4];
 
-uint8_t MIDI_evt_len[256] = {
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x00
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x10
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x20
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x30
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x40
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x50
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x60
-  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x70
-
-  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0x80
-  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0x90
-  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0xa0
-  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0xb0
-
-  2,2,2,2, 2,2,2,2, 2,2,2,2, 2,2,2,2,  // 0xc0
-  2,2,2,2, 2,2,2,2, 2,2,2,2, 2,2,2,2,  // 0xd0
-
-  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0xe0
-
-  0,2,3,2, 0,0,1,0, 1,0,1,1, 1,0,1,0   // 0xf0
-};
-
-	//#define MIDIOUTS(a,b,c) (((uint32_t)c << 16) | ((uint32_t)b << 8) | (uint32_t)a) の逆
+	// (uint32)msg を 4byte に分解
 	messg[0] =   msg       & 0xff;
 	messg[1] =  (msg >> 8) & 0xff;
 	messg[2] =  (msg >> 16) & 0xff;
@@ -169,20 +144,35 @@ uint8_t MIDI_evt_len[256] = {
 	// Acquire a MIDIPacketList
 	uint8_t packetBuf[MIDBUF_SIZE];
 	MIDIPacketList *packetList = (MIDIPacketList *)packetBuf;
-	m_pCurPacket = MIDIPacketListInit(packetList);
+	mid_Packet = MIDIPacketListInit(packetList);
 
-	// Determine the length of msg
-	uint32_t len=MIDI_evt_len[messg[0]];
-	
-
-
+	// length of msg
+	uint32_t len;
+	switch(messg[0] & 0xf0){
+		case 0xc0:
+		case 0xd0:
+			len = 2;
+			break;
+		case 0x80:
+		case 0x90:
+		case 0xa0:
+		case 0xb0:
+		case 0xe0:
+			len = 3;
+			break;
+		case 0xf0:
+			len = 1; //ないと思うけど
+			break;
+		default:
+			len = 0; //ありえないハズ
+	}
 
 	// Add msg to the MIDIPacketList
 	MIDITimeStamp midiTime = mach_absolute_time();
-	MIDIPacketListAdd(packetList, (ByteCount)sizeof(packetBuf), m_pCurPacket, (MIDITimeStamp)midiTime, len, messg);
+	MIDIPacketListAdd(packetList, (ByteCount)sizeof(packetBuf), mid_Packet, (MIDITimeStamp)midiTime, len, messg);
 
 	// Send the MIDIPacketList
-	MIDISend(m_port,m_endpoint,packetList);
+	MIDISend(mid_port,mid_endpoint,packetList);
 
 	return MMSYSERR_NOERROR;
 }
@@ -200,20 +190,21 @@ midiOutLongMsg(HMIDIOUT hmo, LPMIDIHDR pmh, uint32_t cbmh)
 	uint8_t packetBuf[MIDBUF_SIZE];
 	uint32_t pos=0;
 	MIDIPacketList *packetList = (MIDIPacketList *)packetBuf;
-	m_pCurPacket = MIDIPacketListInit(packetList);
+	mid_Packet = MIDIPacketListInit(packetList);
 
 	// Add msg to the MIDIPacketList
 	MIDITimeStamp midiTime = mach_absolute_time();
-	MIDIPacketListAdd(packetList, (ByteCount)sizeof(packetBuf), m_pCurPacket, (MIDITimeStamp)midiTime, pmh->dwBufferLength, (uint8_t *)pmh->lpData);
+	MIDIPacketListAdd(packetList, (ByteCount)sizeof(packetBuf), mid_Packet, 
+				(MIDITimeStamp)midiTime, pmh->dwBufferLength, (uint8_t *)pmh->lpData);
 
-		
 	// Send the MIDIPacketList
-	MIDISend(m_port,m_endpoint,packetList);
+	MIDISend(mid_port,mid_endpoint,packetList);
 
 	MIDI_EXCVWAIT = 1;
 
 	return MMSYSERR_NOERROR;
 }
+
 
 uint32_t
 midiOutUnprepareHeader(HMIDIOUT hmo, LPMIDIHDR pmh, uint32_t cbmh)
