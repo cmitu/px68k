df05aee0e148fca86a04cb10457849de5b9d6262
TurtleBazooka <kameya-z@nifty.com>
2022-09-26 23:00:13 +0900

    WinはMIDI_MAPPERに戻す
    midiOutGetDevCapsがMinGwでうまく使えない・・・とほほ。

--- x68k/midi_win.c
+++ x68k/midi_win.c
@@ -12,27 +12,24 @@
   Listing MIDI device List
 */
 uint32_t
-mid_DevList()
+mid_DevList(LPHMIDIOUT phmo)
 {
-  MIDIOUTCAPS OutCaps;
-  MMRESULT res;
-  uint32_t num, devid;
+
+  uint32_t num;
 
   /* What's No ? */
   num = midiOutGetNumDevs();
 
-  for (devid=0; devid<num; devid++) {
-	/* Get Device information */
-	res = midiOutGetDevCaps(devid, &OutCaps, sizeof(OutCaps));
-	if (res == MMSYSERR_NOERROR) {
-	 //printf("ID=%d: %s\n", devid, outCaps.szPname);
-	 strcpy(menu_items[8][devid], OutCaps.szPname);
-	}
+  /*Open MIDI port(MIDI_MAPPER)*/
+  if (midiOutOpen(phmo, MIDI_MAPPER, 0, 0, CALLBACK_NULL)
+                == MMSYSERR_NOERROR) {
+   midiOutReset(*phmo);
+   /*Store MIDI port*/
+   strcpy(menu_items[8][0], "MIDI_MAPPER");
+   strcpy(menu_items[8][1],"\0"); /* Menu END */
   }
 
-  strcpy(menu_items[8][devid],"\0"); /* Menu END */
-
- return num;
+ return 1;
 }
 
 /*
@@ -42,20 +39,15 @@ void
 midOutChg(uint32_t port_no, uint32_t bank)
 {
 	HMIDIOUT hmo;
+	uint32_t msg;
 
 	/* All note off */
-	for (uint32_t msg=0x7bb0; msg<0x7bc0; msg++) {
-		midiOutShortMsg(hmo, msg);
-	}
-
-	/*Open MIDI port*/
-	if (midiOutOpen(&hOut, port_no, 0, 0, CALLBACK_NULL)
-							== MMSYSERR_NOERROR) {
-	  midiOutReset(hOut);
+	for (msg=0x7bb0; msg<0x7bc0; msg++) {
+	  midiOutShortMsg(hmo, msg);
 	}
 
 	/* select BANK CC20 LSB */
-	uint32_t msg = ((bank << 16) | (0x20 << 8) | 0xb0);/*Bank select2*/
+	msg = ((bank << 16) | (0x20 << 8) | 0xb0);/*Bank select2*/
 	midiOutShortMsg(hmo, msg);
 
  return;
